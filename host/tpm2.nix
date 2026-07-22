52e9ed4917c7ee4830186f1cbb6393c24b1a7d5d
FortiBrine <FortiBrine@users.noreply.github.com>
2026-07-22 15:14:39 +0300

    feat: replace tpm-fido to linux-id

--- host/tpm2.nix
+++ host/tpm2.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  linux-id = pkgs.buildGoModule rec {
+    pname = "linux-id";
+    version = "0.2.3";
+    src = pkgs.fetchFromGitHub {
+      owner = "matejsmycka";
+      repo = "linux-id";
+      rev = "v${version}";
+      hash = "sha256-0lO4lIga/tYzXDOGxYREr2Bgu1P6/3GH67ijivl42D8=";
+    };
+    vendorHash = "sha256-vmWYSlCP09cVgQa7owAZeDzGfEdMHOqQlqDuzTkRjdI=";
+  };
+in
+{
   security.tpm2 = {
     enable = true;
     tctiEnvironment.enable = true;
@@ -14,8 +28,26 @@
   '';
 
   environment.systemPackages = with pkgs; [
-    tpm-fido
+    linux-id
     pinentry-qt
+    tpm2-tools
   ];
 
+  systemd.user.services.linux-id = {
+    description = "linux-id TPM-backed FIDO2/WebAuthn authenticator";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      ExecStart = "${linux-id}/bin/linux-id";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+
+    environment = {
+      PINENTRY_PATH = "${pkgs.pinentry-qt}/bin/pinentry-qt";
+    };
+  };
 }
+
